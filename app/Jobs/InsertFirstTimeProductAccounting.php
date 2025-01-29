3e6c50d727b4306e4b93d4f0853ef0995be83db5
LonelyDeev <mahdi77yousefi2015@gmail.com>
2025-01-29 17:58:18 +0330

    ایجاد کامند برای محصولات جدید به جای آدرس

--- app/Jobs/InsertFirstTimeProductAccounting.php
+++ app/Jobs/InsertFirstTimeProductAccounting.php
@@ -45,7 +45,7 @@ class InsertFirstTimeProductAccounting implements ShouldQueue
             // $response = $client->request('GET', 'http://2.187.99.27:5000/api/products');
             $response = $response->getBody()->getContents();
             $response = json_decode($response, true);
-Log::info($response);
+
             //$productId = Setting::where('key', 'productId')->pluck('value')->first();
             //$productIds = Post::buildCode();
             // if ($response->status == 200) {
@@ -140,5 +140,7 @@ Log::info($response);
             // }
         } catch (\GuzzleHttp\Exception\RequestException $e) {
         }
+
+        option_update('last_time_insertFirstTime_product', now());
     }
 }
