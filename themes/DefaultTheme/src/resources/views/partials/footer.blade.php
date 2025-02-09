e78d2c2eeac26d8a78b0ed3bb7ae718b293becb7
LonelyDeev <mahdi77yousefi2015@gmail.com>
2025-02-09 08:56:47 +0330

    تغییرات جدید

--- themes/DefaultTheme/src/resources/views/partials/footer.blade.php
+++ themes/DefaultTheme/src/resources/views/partials/footer.blade.php
@@ -31,7 +31,7 @@
 
                     <div class="row">
                         <div class="col-12">
-                            <div class="card">
+                            <div class="card widget ">
                                  <p class="text-right" style="color: #666;">
                                 {!! option('about_us_in_footer') !!}
                             </p>
