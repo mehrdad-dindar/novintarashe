c26a6cc131fbb38b0a49e2c910458c930d8de0f1
LonelyDeev <mahdi77yousefi2015@gmail.com>
2025-02-26 14:51:00 +0330

    تغییرات جدید

--- app/Jobs/InsertFirstTimeProductAccounting.php
+++ app/Jobs/InsertFirstTimeProductAccounting.php
@@ -16,133 +16,151 @@ class InsertFirstTimeProductAccounting implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    protected $tries = 5; // کاهش تلاش‌های مجاز برای افزایش امنیت
+    protected $timeout = 300; // کاهش مدت‌زمان تایم‌اوت به 3 دقیقه برای امنیت بیشتر
+
     /**
-     * Create a new job instance.
-     *
-     * @return void
+     * بررسی می‌کند که عدد یک‌رقمی است یا خیر
      */
-    public function __construct()
+    public function isOneDigit($number): bool
     {
-        //
+        return is_numeric($number) && $number >= -9 && $number <= 9;
     }
 
-    public function isOneDigit($number)
+    /**
+     * بررسی تعداد تلاش‌های باقی‌مانده
+     */
+    public function canTryAgain(): bool
     {
-        return ($number >= -9 && $number <= 9);
+        return $this->tries > 0;
     }
 
-    public $tries = 5;
-    public $timeout = 300;
-    
+    /**
+     * کاهش تعداد تلاش‌ها در هر بار اجرای ناموفق
+     */
+    public function reduceTries()
+    {
+        $this->tries--;
+    }
+
     public function handle()
     {
         // if (!checkApiAccounting()) {
         //     return false;
         // }
         $client = new \GuzzleHttp\Client();
+        $page = 1;
+        $perPage = 100;
 
-        try {
-            // $response = $client->request('GET', 'http://127.0.0.1:5000/api/products');
-            $response = $client->request('GET', 'http://109.122.229.114:5000/api/products');
-            // $response = $client->request('GET', 'http://2.187.99.27:5000/api/products');
-            $response = $response->getBody()->getContents();
-            $response = json_decode($response, true);
+        do {
+            try {
+                $response = $client->request('GET', "http://109.122.229.114:5000/api/products", [
+                    'query' => [
+                        'page' => $page,
+                        'per_page' => $perPage
+                    ]
+                ]);
 
-            //$productId = Setting::where('key', 'productId')->pluck('value')->first();
-            //$productIds = Post::buildCode();
-            // if ($response->status == 200) {
-            option_update('last_time_insertFirstTime_product', now());
-            foreach ($response as $article) {
+                $responseBody = json_decode($response->getBody()->getContents(), true);
 
-                if (!Product::where('fldId', $article['A_Code'])->exists()) {
-
-                    $fldId = $article['A_Code'];
-                    $fldC_Kala = $article['A_Code'];
-                    $title = $article['A_Name'];
-                    $vahed_kol = '';
-                    $Vahed = $article['vahed'];
-                    $price = $article['Sel_Price'];
-                    $offPrice = $article['PriceTakhfif'] > 0 ? $article['PriceTakhfif'] : $article['Sel_Price'];
-                    $morePriceArray = [
-                        "fldTipFee1" => $article['Sel_Price'] ?: 0,
-                        "fldTipFee2" => $offPrice ?: 0,
-                        "fldTipFee3" => $article['Sel_Price3'] ?: 0,
-                        "fldTipFee4" => $article['Sel_Price4'] ?: 0,
-                        "fldTipFee5" => $article['Sel_Price5'] ?: 0,
-                        "fldTipFee6" => $article['Sel_Price6'] ?: 0,
-                        "fldTipFee7" => $article['Sel_Price7'] ?: 0,
-                        "fldTipFee8" => $article['Sel_Price8'] ?: 0,
-                        "fldTipFee9" => $article['Sel_Price9'] ?: 0,
-                        "fldTipFee10" => $article['Sel_Price10'] ?: 0,
-                    ];
-
-                    $morePrice = json_encode($morePriceArray);
-                    // $count = explode('/', $article->fldMande);
-                    $count = $article['Exist'];
-
-                    $fldTedadKarton = $article['Karton'];
-                    $status = $article['IsActive'] == "true" ? 1 : 0;
-                    $image = '';
-                    // $fldPorForoosh = $article->fldPorForoosh;
-
-                    $Mcategory = Category::where('fldC_M_GroohKala', $article['Main_Category']['M_groupcode'])->first();
-
-                    $product = new Product();
-                    $product->fldId = $fldId;
-                    $product->fldC_Kala = $fldC_Kala;
-                    $product->title = $title;
-                    $product->slug = $title;
-                    $product->vahed_kol = $vahed_kol;
-                    $product->vahed = $Vahed;
-                    $product->unit = $Vahed ?: $vahed_kol;
-                    $product->morePrice = $morePrice;
-                    $product->fldTedadKarton = $fldTedadKarton;
-                    $product->published = $status;
-                    $product->image = $image;
-                    $product->type = "physical";
-                    $product->currency_id = 1;
-                    $product->category_id = $Mcategory->id;
-                    ///$product->product_id=$productId . '-' . $productIds;
-                    $product->save();
-
-                    if (!empty($article['Main_Category']) && !empty($article['Sub_Category'])) {
-                        $Scategory = Category::where(['fldC_S_GroohKala' => $article['Sub_Category']['S_groupcode'], 'fldC_M_GroohKala' => $article['Main_Category']['M_groupcode']])->first();
-                        $product->categories()->sync([$Mcategory->id, $Scategory->id]);
-                    } else {
-                        $product->categories()->sync([$Mcategory->id]);
-                    }
-
-                    for ($i = 1; $i <= 10; $i++) {
-                        $title = "fldTipFee" . $i;
-                        $fldTipFee = $morePriceArray[$title];
-
-                        $discount = 0;
-                        $discount_price = $fldTipFee;
-                        if ($title == "fldTipFee2") {
-                            $discount = (($price - $offPrice) / $price) * 100;
-                            $discount_price = $offPrice;
-                        }
-                        //$discount=0;
-                        //$discount_price=$fldTipFee;
-                        $product->prices()->create(
-                            [
-                                "title" => $title,
-                                "price" => $fldTipFee,
-                                "discount" => $discount,
-                                "discount_price" => $discount_price,
-                                "stock" => $count,
-                                "stock_carton" => $fldTedadKarton,
-                                "accounting" => 1,
-                            ]
-                        );
-                    }
-                    //$product->$fldPorForoosh=$fldPorForoosh;
+                if (!isset($responseBody['products'])) {
+                    throw new \Exception('Invalid response format');
                 }
-            }
-            // }
-        } catch (\GuzzleHttp\Exception\RequestException $e) {
-        }
 
-        option_update('last_time_insertFirstTime_product', now());
+                option_update('last_time_insertFirstTime_product', now());
+
+                foreach ($responseBody['products'] as $article) {
+                    if (!Product::where('fldId', $article['A_Code'])->exists()) {
+
+                        $fldId = $article['A_Code'];
+                        $fldC_Kala = $article['A_Code'];
+                        $title = $article['A_Name'];
+                        $vahed_kol = '';
+                        $Vahed = $article['vahed'];
+                        $price = $article['Sel_Price'];
+                        $offPrice = $article['PriceTakhfif'] > 0 ? $article['PriceTakhfif'] : $article['Sel_Price'];
+                        $morePriceArray = [
+                            "fldTipFee1" => $article['Sel_Price'] ?: 0,
+                            "fldTipFee2" => $offPrice ?: 0,
+                            "fldTipFee3" => $article['Sel_Price3'] ?: 0,
+                            "fldTipFee4" => $article['Sel_Price4'] ?: 0,
+                            "fldTipFee5" => $article['Sel_Price5'] ?: 0,
+                            "fldTipFee6" => $article['Sel_Price6'] ?: 0,
+                            "fldTipFee7" => $article['Sel_Price7'] ?: 0,
+                            "fldTipFee8" => $article['Sel_Price8'] ?: 0,
+                            "fldTipFee9" => $article['Sel_Price9'] ?: 0,
+                            "fldTipFee10" => $article['Sel_Price10'] ?: 0,
+                        ];
+
+                        $morePrice = json_encode($morePriceArray);
+                        // $count = explode('/', $article->fldMande);
+                        $count = $article['Exist'];
+
+                        $fldTedadKarton = $article['Karton'];
+                        $status = $article['IsActive'] == "true" ? 1 : 0;
+                        $image = '';
+                        // $fldPorForoosh = $article->fldPorForoosh;
+
+                        $Mcategory = Category::where('fldC_M_GroohKala', $article['Main_Category']['M_groupcode'])->first();
+
+                        $product = new Product();
+                        $product->fldId = $fldId;
+                        $product->fldC_Kala = $fldC_Kala;
+                        $product->title = $title;
+                        $product->slug = $title;
+                        $product->vahed_kol = $vahed_kol;
+                        $product->vahed = $Vahed;
+                        $product->unit = $Vahed ?: $vahed_kol;
+                        $product->morePrice = $morePrice;
+                        $product->fldTedadKarton = $fldTedadKarton;
+                        $product->published = $status;
+                        $product->image = $image;
+                        $product->type = "physical";
+                        $product->currency_id = 1;
+                        $product->category_id = $Mcategory->id;
+                        ///$product->product_id=$productId . '-' . $productIds;
+                        $product->save();
+
+                        if (!empty($article['Main_Category']) && !empty($article['Sub_Category'])) {
+                            $Scategory = Category::where(['fldC_S_GroohKala' => $article['Sub_Category']['S_groupcode'], 'fldC_M_GroohKala' => $article['Main_Category']['M_groupcode']])->first();
+                            $product->categories()->sync([$Mcategory->id, $Scategory->id]);
+                        } else {
+                            $product->categories()->sync([$Mcategory->id]);
+                        }
+
+                        for ($i = 1; $i <= 10; $i++) {
+                            $title = "fldTipFee" . $i;
+                            $fldTipFee = $morePriceArray[$title];
+
+                            $discount = 0;
+                            $discount_price = $fldTipFee;
+                            if ($title == "fldTipFee2") {
+                                $discount = (($price - $offPrice) / $price) * 100;
+                                $discount_price = $offPrice;
+                            }
+                            //$discount=0;
+                            //$discount_price=$fldTipFee;
+                            $product->prices()->create(
+                                [
+                                    "title" => $title,
+                                    "price" => $fldTipFee,
+                                    "discount" => $discount,
+                                    "discount_price" => $discount_price,
+                                    "stock" => $count,
+                                    "stock_carton" => $fldTedadKarton,
+                                    "accounting" => 1,
+                                ]
+                            );
+                        }
+                        //$product->$fldPorForoosh=$fldPorForoosh;
+                    }
+                }
+
+                $page++;
+                // }
+            } catch (\GuzzleHttp\Exception\RequestException $e) {
+            }
+        } while ($page <= $responseBody['pagination']['total_pages']);
+
     }
 }
