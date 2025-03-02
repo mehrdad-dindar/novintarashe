fba6e75827eecbb12c5d90403b5af8b6683227e9
LonelyDeev <mahdi77yousefi2015@gmail.com>
2025-03-02 17:33:53 +0330

    تغییرات جدید

--- themes/DefaultTheme/src/Controllers/SitemapController.php
+++ themes/DefaultTheme/src/Controllers/SitemapController.php
@@ -39,7 +39,7 @@ class SitemapController extends Controller
             );
         }
 
-        return $sitemap->render();
+        return $sitemap->render('xml');
     }
 
     public function posts()
@@ -59,7 +59,7 @@ class SitemapController extends Controller
             }
         }
 
-        return $sitemap->render();
+        return $sitemap->render('xml');
     }
 
     public function pages()
@@ -79,7 +79,7 @@ class SitemapController extends Controller
             }
         }
 
-        return $sitemap->render();
+        return $sitemap->render('xml');
     }
 
     public function products()
@@ -101,7 +101,7 @@ class SitemapController extends Controller
             }
         }
 
-        return $sitemap->render();
+        return $sitemap->render('xml');
 
     }
 
@@ -128,6 +128,6 @@ class SitemapController extends Controller
             }
         }
 
-        return $sitemap->render();
+        return $sitemap->render('xml');
     }
 }
