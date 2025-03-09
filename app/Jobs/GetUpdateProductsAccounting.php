f17da786863d587366277a4ffd3edba139158857
LonelyDeev <mahdi77yousefi2015@gmail.com>
2025-03-09 20:01:25 +0330

    تغییرا جدید

--- app/Jobs/GetUpdateProductsAccounting.php
+++ app/Jobs/GetUpdateProductsAccounting.php
@@ -12,6 +12,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
 use Log;
 
 class GetUpdateProductsAccounting implements ShouldQueue
