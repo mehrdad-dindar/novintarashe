e7823c8d79fa3b54bcb4dd56d827d3452b04b45d
LonelyDeev <mahdi77yousefi2015@gmail.com>
2025-06-01 22:39:29 +0330

    تغییرات جدید

--- themes/DefaultTheme/src/resources/views/auth/layouts/master.blade.php
+++ themes/DefaultTheme/src/resources/views/auth/layouts/master.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="theme-color" content="#f7858d">
+     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="msapplication-navbutton-color" content="#f7858d">
     <meta name="apple-mobile-web-app-status-bar-style" content="#f7858d">
     <title>
