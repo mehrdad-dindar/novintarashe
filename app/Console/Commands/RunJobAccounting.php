78456e8e3e9a3a0a5d216bff4ebd55315147e5ae
LonelyDeev <mahdi77yousefi2015@gmail.com>
2025-06-05 19:45:05 +0330

    تغییرات جدید

--- app/Console/Commands/RunJobAccounting.php
+++ app/Console/Commands/RunJobAccounting.php
@@ -26,9 +26,9 @@ class RunJobAccounting extends Command
     {
         Artisan::call('queue:work --queue=get_category_accounting --stop-when-empty');
         Artisan::call('queue:work --queue=send-order-accounting --stop-when-empty');
-        Artisan::call('queue:work --queue=insert_first_time_product_accounting --stop-when-empty');
-        Artisan::call('queue:work --queue=get_new_product_accounting --stop-when-empty');
-        Artisan::call('queue:work --queue=get_update_product_accounting --stop-when-empty');
+        //Artisan::call('queue:work --queue=insert_first_time_product_accounting --stop-when-empty');
+        //Artisan::call('queue:work --queue=get_new_product_accounting --stop-when-empty');
+        //Artisan::call('queue:work --queue=get_update_product_accounting --stop-when-empty');
         Artisan::call('queue:work --queue=send_happy_birthday --stop-when-empty');
         Artisan::call('queue:work --queue=apply_currency_to_all_products --stop-when-empty');
     }
