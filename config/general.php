b082c2fbf26128e70261c5b3fedd9e6f181d5077
Mehrdad Dindar <mehrdad.replay@gmail.com>
2025-10-15 08:20:47 +0330

    add SEP config

--- config/general.php
+++ config/general.php
@@ -317,6 +317,12 @@ return [
                 'merchantId' => 'کد پذیرنده'
             ]
         ],
+        'sep'       => [
+            'label' => 'درگاه سامان کیش',
+            'fields' => [
+                'merchantId' => 'کد پذیرنده'
+            ]
+        ],
         'sepehr'      => [
             'label' => 'درگاه سپهر (بانک صادرات)',
             'fields' => [
